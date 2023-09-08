b643aa76aee47b424cc880d433bb6de98b341ebd
Brandonmejia19 <brandonmejia887@gmail.com>
2023-09-08 15:38:27 -0600

    Brandon

---
+++ lib/pages/brandonmejia.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BrandonMejia extends StatelessWidget {
+  const BrandonMejia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Archivo Brandon'),
+      ),
+      body: Column(
+        children: [
+          Text('Fichero de Brandon')
+        ],
+      ),
+    );
+  }
+}
